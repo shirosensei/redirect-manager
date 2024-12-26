b4e6084a2ea6f613fd3c104ea5cb7cc004af49b7
Tozo sensei <codersensei@outlook.com>
2024-12-26 10:31:02 -0800

    fix(Login.jsx): fix the sign up button

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h2>Admin Login</h2>
+      <h2> Login</h2>
       <form onSubmit={handleSubmit} className="form">
         <div>
           <label>Username:</label>
